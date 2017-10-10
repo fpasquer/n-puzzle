5322dc5046c46dfb258e2c93d29e9f1861ae7bb2
Florian PASQUEREAU <fpasquer@e1r5p12.42.fr>
2017-10-10 14:03:38 +0200

    main normed

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: fpasquer <fpasquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/28 17:26:06 by fpasquer          #+#    #+#             */
-/*   Updated: 2017/09/20 11:26:50 by fpasquer         ###   ########.fr       */
+/*   Updated: 2017/10/10 13:59:00 by fpasquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 bool						print_flag(unsigned int const flag)
 {
-	char					non[] = "\033[31mNon\033[0m";
-	char					oui[] = "\033[32mOui\033[0m";
-
-	printf("Manhattan       %s\n", (flag & F_MANHATTAN) == 0 ? non : oui);
-	printf("Linear conflict %s\n", (flag & F_LINEAR_C) == 0 ? non : oui);
-	printf("Wrong place     %s\n", (flag & F_MAL_PLACE) == 0 ? non : oui);
+	printf("Manhattan       %s\n", (flag & F_MANHATTAN) == 0 ?
+			"\033[31mNon\033[0m" : "\033[32mOui\033[0m");
+	printf("Linear conflict %s\n", (flag & F_LINEAR_C) == 0 ?
+			"\033[31mNon\033[0m" : "\033[32mOui\033[0m");
+	printf("Wrong place     %s\n", (flag & F_MAL_PLACE) == 0 ?
+			"\033[31mNon\033[0m" : "\033[32mOui\033[0m");
 	return (true);
 }
 
@@ -36,22 +36,43 @@ void						start_loop(t_grid *grid, t_coord coord_zero,
 	right_coord = is_Done(grid);
 	weight = grid->max_deep;
 	printf("weight manhattan + linear_conflict = %d\n", weight);
-	for (start = clock(); grid->max_deep < MAX_DEEP &&
-			grid->found == false; grid->max_deep++)
+	start = clock();
+	while (grid->max_deep < MAX_DEEP && grid->found == false)
+	{
 		solve_npuzzle(grid, 0, coord_zero.y, coord_zero.x,
 				get_move_possible(grid, coord_zero.y, coord_zero.x),
 				right_coord, weight);
+		grid->max_deep++;
+	}
 	printf("Time = %f\nMemory = %zu octets\n", (double)(clock() - start) /
 			CLOCKS_PER_SEC, sizeof(*grid) * grid->max_deep);
 	show_way(grid, &file, fd, coord_zero.y, coord_zero.x);
 }
 
-int							main(int argc, char **argv)
+static void					main_next(int const fd, int const flags)
 {
 	char					**file;
-	int						x_y, fd, flags;
-	t_coord					coord_zero;
+	int						x_y;
 	t_grid					*grid;
+	t_coord					coord_zero;
+
+	if (fd <= 0)
+		return ;
+	if ((file = save_file(fd)) != NULL)
+	{
+		if ((x_y = check_file(file)) > 0)
+			if ((grid = get_grid(file, x_y, flags)) != NULL &&
+					get_coord_zero(grid, &coord_zero) == true)
+				if (solvable(grid) == true)
+					start_loop(grid, coord_zero, file, fd);
+		del_file(&file);
+	}
+}
+
+int							main(int argc, char **argv)
+{
+	int						fd;
+	int						flags;
 
 	flags = 0;
 	if (argc <= 1 && generate_grid() == false)
@@ -61,15 +82,7 @@ int							main(int argc, char **argv)
 	}
 	if ((fd = check_argv(argv, &flags)) > 1 && print_flag(flags) == true)
 	{
-		if ((file = save_file(fd)) != NULL)
-		{
-			if ((x_y = check_file(file)) > 0)
-				if ((grid = get_grid(file, x_y, flags)) != NULL &&
-						get_coord_zero(grid, &coord_zero) == true)
-					if (solvable(grid) == true)
-						start_loop(grid, coord_zero, file, fd);
-			del_file(&file);
-		}
+		main_next(fd, flags);
 		close(fd);
 	}
 	ft_putstr_fd("Error\n", STDERR_FILENO);
